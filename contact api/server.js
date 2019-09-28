8f718387f019f58ad99c6fb7f1e71ecc184532a9
Audrey <audreyndum@gmail.com>
2019-09-28 16:39:31 +0100

    modified server.js and heroku configurations

--- contact api/server.js
+++ contact api/server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/contact-api'));
+app.use(express.static(__dirname + '/dist/routing-app'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/contact-api/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/routing-app/index.html'));
 });
 
 // default Heroku PORT
