537a962a0b2e5d95a4a41575f4217c11f1726018
Audrey <audreyndum@gmail.com>
2019-09-28 16:49:16 +0100

    modified backend link to include secure https response

--- contact api/src/app/contact.service.ts
+++ contact api/src/app/contact.service.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class ContactService {
-  private _url = 'http://gg-trainee-contacts-api.herokuapp.com/api/contacts';
+  private _url = 'https://gg-trainee-contacts-api.herokuapp.com/api/contacts';
 
   getContacts() {
     return this.http.get(this._url);
